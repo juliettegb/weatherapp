f5e4b0bb210181fc5d6400629a4ab21c151d2908
Juliette Grandin <juliette.grandin@gmail.com>
2017-11-03 17:53:11 +0100

    map center sur dernier element

---
+++ public/script.js
@@ -0,0 +1,70 @@
+$(function() { //permet de temporiser l'exécution car sinon on ferait appel à des éléments html qui n'existent même pas encore
+  $("#sortable").sortable();
+  $("#sortable").disableSelection();
+
+  $("#sortable").on("sortupdate", function(event, ui) { // on n'exploite pas event et placeholder ici donc on peut même les enlever en fait! et habituellement on met ui plutôt que placeholder
+    var sortedIDs = $("#sortable").sortable("toArray"); // to array: nécessite de donner IDs uniques à nos éléments
+    console.log(sortedIDs);
+    var updatedcityListString = JSON.stringify(sortedIDs);
+    // console.log(updatedcityListString); //nouvel ordre des villes (ou bien nouvelle position si id = i)
+
+    $.getJSON("/update?listemaj="+updatedcityListString, function(data) {
+      if(data.result == true){
+        $(".list-group-item").each(function(index){ //index donne le num du li sur lequel on se trouve & each: fonction de jquery = à for mais plus simple
+      //  $(this).attr("id", index); //on boucle dans les li, this = moi, le li sur lequel on se trouve ; après "id" = nouvelle valeur à donner : seulement nécessaire quand id=i
+          $(this).find("a").attr("href", "/delete?position="+index ) //car sinon bug sur le bouton supprimer qui supprimait l'ancienne position associée
+        });
+      }
+    });
+  });
+
+/*  $("#city").keypress(function(){
+    var entree = $("#city");
+    console.log("Entrée détectée: "+ entree);
+  });
+
+
+  $("#city").on("keypress", function(event){
+    console.log("Entrée: "+ event.which);
+  }); */
+
+});
+
+var autocomplete
+function initAutocomplete() { //Google n'accepte qu'une fonction de rappel dans l'url (cf ejs) donc on a tout mis dans une seule fonction, on aurait pu changer le nom genre autocompleteandmap
+  // Create the autocomplete object, restricting the search to geographical
+  // location types.
+  autocomplete = new google.maps.places.Autocomplete(
+      /* @type {!HTMLInputElement} */(document.getElementById('city')),
+  );
+
+  var Paris = {lat: 48.866667, lng: 2.333333};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: Paris,
+  });
+
+  $("li").each(function(){
+    var lat = $(this).data("lat");
+    var lon = $(this).data("lon");
+    var location = {lat: lat, lng: lon};
+    var marker = new google.maps.Marker({
+      position: location,
+      map: map,
+    });
+    map.panTo(location);
+  });
+};
+
+/*
+1st step:
+Capter la saisie (on déclenche la demande dès qu'une lettre est saisie) -->
+apporter une écoute sur un signal qui va nous permettre de savoir qu'une
+nouvelle lettre a été renseignée. Récupérer input = blablaquivarie.
+
+Envoyer l'ordre, la question au webservice (ajax)
+
+Boucle : afficher info dans la console du nav (que nom ville, pas pays)
+
+https://maps.googleapis.com/maps/api/place/autocomplete/json?key=AIzaSyALD-Dfvhh3rW5r6twUYDyz_T75QMHsDcU&components=country:fr&input=pa
+*/
--- serveur.js
+++ serveur.js
@@ -18,6 +18,7 @@ var app = express(); // initialisation du serveur
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+var cityList = [];
 
 var options = { server: { socketOptions: {connectTimeoutMS: 30000 } }};
 mongoose.connect('mongodb://weatherapp:test@ds241395.mlab.com:41395/weatherapp', options, function(err){
@@ -30,15 +31,18 @@ var citySchema = mongoose.Schema({
   icon: String,
   tempMax: String,
   tempMin: String,
+  position: Number,
+  lat: Number,
+  lon: Number,
 });
 
 var cityModel = mongoose.model('Cities', citySchema); // lecteur enregistreur prêt, il a les règles à respecter etc.
 
-var cityList = [];
-
 app.get('/', function(req, res){
   //res.render('Liste', {cityList}); //plus besoin, à la place on met le cityModel.find pour s'affranchir de cityList et plutôt utiliser la BDD
-  cityModel.find(function (err, datas){ //pas de filtre, on veut tout afficher
+  var query = cityModel.find();
+  query.sort({position: 1});
+  query.exec(function (err, datas){ //pas de filtre, on veut tout afficher
     res.render('Liste', {cityList: datas});
   });
 });
@@ -47,9 +51,9 @@ app.get("/add", function(req, res){
 
   request("http://api.openweathermap.org/data/2.5/weather?APPID=74f8a59f951845ccaeea31a9b0c9ae11&lang=fr&units=metric&q="+req.query.city+"", function(error, response, body){
     body = JSON.parse(body);
-    var newcity = {name: body.name, desc: body.weather[0].description, icon: "http://openweathermap.org/img/w/"+body.weather[0].icon+".png", tempMax: body.main.temp_max, tempMin: body.main.temp_min};
+    // var newcity = {name: body.name, desc: body.weather[0].description, icon: "http://openweathermap.org/img/w/"+body.weather[0].icon+".png", tempMax: body.main.temp_max, tempMin: body.main.temp_min};
     //cityList.push(newcity);
-    console.log(body);
+    // console.log(body);
     /* console.log("http://openweathermap.org/img/w/"+body.weather[0].icon+".png")
     console.log(req.query.city);
     console.log(body.name);
@@ -57,51 +61,67 @@ app.get("/add", function(req, res){
     console.log(body.weather[0].icon);
     console.log(body.main.temp_max);
     console.log(body.main.temp_min); */
-    var city = new cityModel({ //ordre d'enregistrement
-    name: body.name,
-    desc: body.weather[0].description,
-    icon: "http://openweathermap.org/img/w/"+body.weather[0].icon+".png",
-    tempMax: body.main.temp_max +"°C",
-    tempMin: body.main.temp_min +"°C",
-  });
 
-  city.save(function (error, city){ //on est sur de l'asynchrone donc fonction de callback qui sera exécutée lorsque le backend aura fini son boulot!
-  console.log(error);
-  console.log(city);
-  cityModel.find(function (err, datas){ //pas de filtre, on veut tout afficher
-    res.render('Liste', {cityList: datas});
-  });
-});
-});
+    var query = cityModel.find(); // on stocke le find dans query pour pouvoir l'exécuter que plus tard via .exec
+    query.sort({position: 1}); // Sort datas par position croissante
 
+    query.exec(function(err,datas){
+
+      var nextPosition = 1 //car si pas de données dans notre BDD, il manque une info pour attribuer la position à notre nouvelle ville
+      if (datas.length>0){
+        nextPosition = (datas[datas.length-1].position)+1
+      }
+
+      var city = new cityModel({ //ordre d'enregistrement
+        name: body.name,
+        desc: body.weather[0].description,
+        icon: "http://openweathermap.org/img/w/"+body.weather[0].icon+".png",
+        tempMax: body.main.temp_max +"°C",
+        tempMin: body.main.temp_min +"°C",
+        position: nextPosition, //-1 car on est dans les tiroirs du tableau, pas dans la position sur la BDD
+        lat: body.coord.lat,
+        lon: body.coord.lon,
+      });
+
+      city.save(function (error, city){ //on est sur de l'asynchrone donc fonction de callback qui sera exécutée lorsque le backend aura fini son boulot!
+        console.log(error);
+        console.log(city);
+        cityModel.find(function (err, datas){ //on refait un find pour que le res.render affiche également les nouvelles entrées
+          res.render('Liste', {cityList: datas});
+        });
+      });
+
+    });
+  });
 });
 
 app.get("/delete", function(req, res){
   cityModel.remove({_id: req.query.id}, function (err,datas){
     console.log(req.query.id);
     cityModel.find(function (err, datas){
-      res.render('Liste', {cityList: datas});
+      res.render('Liste', {cityList: datas}); // on déverse datas dans cityList, cityList n'est plus notre var d'avant mais bien cette fois la cityList rendue accessible dans l'EJS via la BDD
     });
   });
 });
 
-// Pb de la solution avec id = nom de ville, si j'ai deux fois le même nom l'ordre va en être affecté ! Voir autre solution avec id = position ou autre id unique avec timestamp (?)
-app.get("/update", function(req, res){
-  var updatedcityList = JSON.parse(req.query.listemaj); // pour "annuler" le JSON.stringify & pour info on aurait pu mettre var updatedcityList = JSON.parse(req.query.listemaj)
-  console.log(updatedcityList);
-  var cityListTmp = []; // Liste temporaire à laquelle j'ajoute la valeur du tiroir que je viens de tirer
-  for (var i=0; i<updatedcityList.length; i++){
-    for (var j=0; j<cityList.length; j++){
-      if (updatedcityList[i] == cityList[j].name){
-        cityListTmp.push(cityList[j]);
-      }
-    }
-  }
+app.get("/update", function(req,res){
+  var updatedcityList = JSON.parse(req.query.listemaj);
+  var query = cityModel.find(); // on stocke le find dans query pour pouvoir l'exécuter que plus tard via .exec
+  query.exec(function(err,datas){
+    //query.update(function(err,datas){
+      for (var i=0; i<updatedcityList.length; i++){
+        for (var j=0; j<datas.length; j++){
+          if (updatedcityList[i] == datas[j].name){
+            datas[j].position = i;
+            cityModel.update({name: datas[j].name}, {position: i}, function(){});
+          }
+        };
+      //};
+    };
+  });
 
-  cityList = cityListTmp; // pour écraser l'ancien ordre de la liste
-  res.send({result : true}); // on met juste un "ok" car on n'exploite pas le ajax ici donc c'est plus léger de juste envoyer ça
+  res.send({resultat: true});
 });
-
 
 app.listen(8080, function(){
   console.log('Server listening on port 8080');
